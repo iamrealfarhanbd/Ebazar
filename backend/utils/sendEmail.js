a394db98d9ae5ab2ecfcbfc985ec7b1f4ef3396c
unknown <iamfarhan09@gmail.com>
2022-03-02 02:46:36 +0600

    check css

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,9 +1,17 @@
 const nodeMailer = require("nodemailer");
 
 const sendEmail = async (options) => {
-  const transporter = nodeMailer.createTransport({
-    host: process.env.SMPT_HOST,
-    port: process.env.SMPT_PORT,
+  // const transporter = nodeMailer.createTransport({
+  //   host: process.env.SMPT_HOST,
+  //   port: process.env.SMPT_PORT,
+  //   service: process.env.SMPT_SERVICE,
+  //   auth: {
+  //     user: process.env.SMPT_MAIL,
+  //     pass: process.env.SMPT_PASSWORD,
+  //   },
+  // });
+
+  var transporter = nodeMailer.createTransport({
     service: process.env.SMPT_SERVICE,
     auth: {
       user: process.env.SMPT_MAIL,
@@ -11,14 +19,29 @@ const sendEmail = async (options) => {
     },
   });
 
-  const mailOptions = {
+  // const mailOptions = {
+  //   from: process.env.SMPT_MAIL,
+  //   to: options.email,
+  //   subject: options.subject,
+  //   text: options.message,
+  // };
+
+  var mailOptions = {
     from: process.env.SMPT_MAIL,
     to: options.email,
     subject: options.subject,
     text: options.message,
   };
 
-  await transporter.sendMail(mailOptions);
+  // await transporter.sendMail(mailOptions);
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
 };
 
 module.exports = sendEmail;
